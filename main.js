9736a093bbe421be96cb7660d1d21498a66ca832
Alberto Arias <alberto.arias.x64@gmail.com>
2023-10-14 18:37:51 -0500

    canvas

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 import * as THREE from 'three';
 
+// Scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const canvas = document.querySelector("#canva_1")
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// Camera
+const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+// Render
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+
+//Geometries
+const geometry = new THREE.CylinderGeometry(1, 1, 1, 30)
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 6;
 
 function animate() {
     requestAnimationFrame(animate);
