057b0bf8104e12fb76d8be06c839672623a92a70
Alberto Arias <alberto.arias.x64@gmail.com>
2023-10-15 00:28:16 -0500

    change speed animation

--- main.js
+++ main.js
@@ -23,8 +23,8 @@ camera.position.z = 6;
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.02;
+    cube.rotation.y += 0.02;
     renderer.render(scene, camera);
 }
 animate();
